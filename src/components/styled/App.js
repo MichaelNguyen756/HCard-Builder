a460a3125ba5ab01a41928e3dabdab488065d98d
Michael Nguyen <michael.nguyen756@gmail.com>
2020-08-11 02:38:14 +1000

    Minor css adjustments

--- src/components/styled/App.js
+++ src/components/styled/App.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 const App = styled.div`
     display: flex;
     flex-flow: row nowrap;
+    box-sizing: border-box;
+    height: 100vh;
 `;
 
 export default App;
--- src/components/styled/BuilderSection.js
+++ src/components/styled/BuilderSection.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 const BuilderSection = styled.section`
     padding: 1rem 0.8rem;
     flex: 9 9 45%;
+    height: 100vh;
+    box-sizing: border-box;
 `;
 
 export default BuilderSection;
