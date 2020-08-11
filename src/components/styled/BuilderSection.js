91b14d7059c9c93e0896f99dcd1297e0dc1fe5c6
Michael Nguyen <michael.nguyen756@gmail.com>
2020-08-12 02:10:52 +1000

    Added responsive design for landscape screens

--- src/components/styled/BuilderSection.js
+++ src/components/styled/BuilderSection.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 const BuilderSection = styled.section`
     padding: 1rem 0.8rem;
-    flex: 9 9 45%;
     height: 100vh;
     box-sizing: border-box;
+    width: 40vw;
+
+    @media screen and (min-width: 1024px) {
+        width: 45vw;
+    }
 `;
 
 export default BuilderSection;
--- src/components/styled/HCardPreviewSection.js
+++ src/components/styled/HCardPreviewSection.js
@@ -2,16 +2,20 @@ import styled, { css } from 'styled-components';
 
 const HCardPreviewSection = styled.section`
     padding: 1rem 1.1rem;
-    flex: 11 11 55%;
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     box-sizing: border-box;
+    width: 60vw;
 
     ${({ backgroundColour }) => css`
         background-color: ${backgroundColour};
     `}
+
+    @media screen and (min-width: 1024px) {
+        width: 55vw;
+    }
 `;
 
 export default HCardPreviewSection;
